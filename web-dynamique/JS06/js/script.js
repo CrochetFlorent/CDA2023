11c16807127daea8c3955402328a8e71abc1ab00
CrochetFlorent <crochet.florent@gmail.com>
2024-02-05 16:18:59 +0100

    commit du soir

---
+++ web-dynamique/JS06/js/script.js
@@ -0,0 +1,35 @@
+
+function setTwoNumberDecimal(number) {
+    number = parseFloat(number).toFixed(2);
+    return number;
+
+}
+function setValuesOnLoad()
+{
+    document.querySelector('input[name="quantite1"]').value = 1;
+    document.querySelector('input[name="quantite2"]').value = 1;
+    document.querySelector('input[name="prix-unitaire1"]').value = 0;
+    document.querySelector('input[name="prix-unitaire2"]').value = 0;
+    document.querySelector('input[name="prix1"]').value = 0;
+    document.querySelector('input[name="prix2"]').value = 0;
+    document.querySelector('input[name="resultat"]').value = 0;
+}
+
+function setValuesFocusOut()
+{
+    document.querySelector(('input[name="prix1"]')).value = 
+    document.querySelector('input[name="quantite1"]').value * document.querySelector('input[name="prix-unitaire1"]').value;
+    document.querySelector(('input[name="prix1"]')).value = setTwoNumberDecimal(document.querySelector(('input[name="prix1"]')).value);
+
+    document.querySelector(('input[name="prix2"]')).value = 
+    document.querySelector('input[name="quantite2"]').value * document.querySelector('input[name="prix-unitaire2"]').value;
+    document.querySelector(('input[name="prix2"]')).value = setTwoNumberDecimal(document.querySelector(('input[name="prix2"]')).value);
+
+    document.querySelector('input[name="resultat"]').value = 
+    document.querySelector(('input[name="prix1"]')).value*1 + document.querySelector(('input[name="prix2"]')).value*1;
+}
+
+
+
+document.addEventListener("DOMContentLoaded",setValuesOnLoad);
+document.addEventListener("focusout",setValuesFocusOut);
---
+++ web-dynamique/JS09/js/script.js
@@ -0,0 +1,75 @@
+
+let rouge;
+let vert;
+let bleu;
+
+
+
+function lireCouleur(){
+    chaineRouge = document.getElementById("rouge").value;
+    chaineVerte = document.getElementById("vert").value;
+    chaineBleu = document.getElementById("bleu").value;
+
+    chaineCouleur = "#" + chaineRouge + chaineVerte + chaineBleu;
+    /*document.body.style.background = chaineCouleur;*/
+    document.body.setAttribute("style", "background-color:" + chaineCouleur);
+}
+
+    document.addEventListener("DOMContentLoaded", lireCouleur);
+    document.addEventListener("focusout",function(event){
+            switch (event.target.name)
+            {
+                case "couleur1":
+                    if(!event.target.checkValidity())
+                    {
+                        event.target.value = chaineRouge;   
+                        return;
+                    }
+                break;
+
+                case "couleur2":
+                    if(!event.target.checkValidity())
+                    {
+                        event.target.value = chaineVerte;
+                        return;
+                    }
+                break;
+
+                case "couleur3":
+                    if(!event.target.checkValidity())
+                    {
+                        event.target.value = chaineBleu;
+                        return;
+                    }
+                break;
+
+            }
+            lireCouleur();
+    });
+
+    monBtnRouge = document.querySelector("#rouge-button");
+    monBtnRouge.addEventListener("click",function(){  
+        document.getElementById("rouge").value = "FF";
+        document.getElementById("vert").value = "00";
+        document.getElementById("bleu").value = "00";
+        lireCouleur();
+    });
+
+    monBtnRouge = document.querySelector("#vert-button");
+    monBtnRouge.addEventListener("click",function(){  
+        document.getElementById("rouge").value = "00";
+        document.getElementById("vert").value = "FF";
+        document.getElementById("bleu").value = "00";
+        lireCouleur();
+    });
+
+    monBtnRouge = document.querySelector("#bleu-button");
+    monBtnRouge.addEventListener("click",function(){  
+        document.getElementById("rouge").value = "00";
+        document.getElementById("vert").value = "00";
+        document.getElementById("bleu").value = "FF";
+        lireCouleur();
+    });
+
+
+    
