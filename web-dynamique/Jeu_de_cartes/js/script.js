95df523747055822a1f69a08c38ff4cd63d58c82
CrochetFlorent <crochet.florent@gmail.com>
2024-02-16 11:56:02 +0100

    Commit du soir

--- web-dynamique/Jeu_de_cartes/js/script.js
+++ web-dynamique/Jeu_de_cartes/js/script.js
@@ -7,26 +7,49 @@ function recupererJSON(){
 }
 recupererJSON();
 
-
-
-function remplirTableau(_data){
+function remplirTableau(_data) {
     let body = document.querySelector('#tab');
 
-    let mytable=document.createElement("table");
+    let mytable = document.createElement("table");
+    mytable.setAttribute("id","tab_jeu_de_cartes");
 
     let myTHead = mytable.createTHead();
     let myFirstRow = myTHead.insertRow();
+    let mykeys = [];
+
+    for (const key in _data[0]) {
+        let myCell = document.createElement('th');
+
+        myCell.textContent = key;
+        mykeys.push(key);
+        myFirstRow.appendChild(myCell);
+
+    }
+
+    let myTBody = mytable.createTBody();
+    let mySecondRow = myTBody.insertRow();
+    for (const key in _data[0]) {
 
 
-    
-    
-        for (const key in _data[0]) {
-            let myCell  = document.createElement('th');
-            myCell.textContent = key;
-            myFirstRow.appendChild(myCell);
-        }    
-    
+        let myCell = mySecondRow.insertCell();
+        myCell.textContent = _data[0][key];
+    }
 
+    for (let i = 1; i < _data.length; i++) {
+        let myObj = _data[i];
+        let myNextRow = myTBody.insertRow();
+
+        for (let j = 0; j < mykeys.length; j++) {
+
+            let myCell = myNextRow.insertCell();
+            if (mykeys[j] != "description") {
+                myCell.textContent = myObj[mykeys[j]];
+            }
+            else {
+                myCell.textContent = "";
+            }
+        }
+    }
     body.appendChild(mytable);
 
 }
